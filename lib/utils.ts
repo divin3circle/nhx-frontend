3167e06580c1decf67cef0ca29a7fa84b0313f5e
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-11-13 13:21:27 +0300

    feat: integrate @hashgraph/sdk for transaction execution and status updates, enhancing mint approval process and improving transaction handling in the admin dashboard

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,32 @@
+import { AccountId, Client, PrivateKey } from "@hashgraph/sdk";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function setUpClient(): Client {
+  let client: Client;
+  const ACCOUNT_ID = AccountId.fromString("0.0.6492202");
+  const KEY = PrivateKey.fromStringECDSA(
+    "0x33d664a003623ffff9e7fda83369301b86653de4ead1ba99002d4bb55e34efe3"
+  );
+
+  client = Client.forTestnet();
+
+  client.setOperator(ACCOUNT_ID, KEY);
+  return client;
+}
+
+export function getSupplyPrivateKey(): PrivateKey {
+  return PrivateKey.fromStringECDSA(
+    "0x33d664a003623ffff9e7fda83369301b86653de4ead1ba99002d4bb55e34efe3"
+  );
+}
+
+export function getAccountId(): AccountId {
+  return AccountId.fromString("0.0.6492202");
 }
 
 export const API_URL = "https://nhx-server.orcus-finance.space/api/v1";
@@ -12,4 +36,5 @@ export const HEDERA_URL = "https://testnet.mirrornode.hedera.com/api/v1";
 export const HEDERA_KESY_BALANCES_URL =
   "https://testnet.mirrornode.hedera.com/api/v1/tokens/0.0.7228867/balances?account.publickey=3c3d546321ff6f63d701d2ec5c277095874e19f4a235bee1e6bb19258bf362be&order=asc";
 export const KESY_TOKEN_ID = "0.0.7228867";
+
 export const DECIMALS = 6;
