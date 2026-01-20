cbb759abbb2493f12f04d4749077e5951e91d3fb
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2026-01-20 10:34:28 +0300

    feat: add admin path rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "admin.nhx.finance" }],
+        destination: "/kesy/admin/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
